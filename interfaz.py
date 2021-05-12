4663ff105f6683e6cbc4d644b2975e248d1ee30d
Felipe <felipeobando@estudianTEC.cr>
2021-05-12 11:55:20 -0600

    Confirmar justificación RAAA

--- interfaz.py
+++ interfaz.py
@@ -1,4 +1,5 @@
 #Este código es para la selección de provincia, la misma lógica sirve para seleccionar el sexo y la justificación.
+"""
 from tkinter import *
 from funciones import *
 from tkinter import messagebox
@@ -16,15 +17,12 @@ opcion = IntVar()
 def escogido():
     escogio=opcion.get()
     print(escogio)
-Radiobutton(root, text="1. San José", variable=opcion,value=1,command=seleccionar).pack()
-Radiobutton(root, text="2. Alajuela", variable=opcion,value=2, command=seleccionar).pack()
-Radiobutton(root, text="3. Cartago", variable=opcion,value=3, command=seleccionar).pack()   
-Radiobutton(root, text="4. Cartago", variable=opcion,value=4, command=seleccionar).pack()   
-Radiobutton(root, text="5. Guanacaste", variable=opcion,value=5, command=seleccionar).pack()   
-Radiobutton(root, text="6. Puntarenas", variable=opcion,value=6, command=seleccionar).pack()   
-Radiobutton(root, text="7. Limón.¿", variable=opcion,value=7, command=seleccionar).pack()   
-Radiobutton(root, text="8. Nacionalizado o naturalizado", variable=opcion,value=8, command=seleccionar).pack()   
-Radiobutton(root, text="9. Caso especial", variable=opcion,value=9,command=seleccionar).pack()   
+Radiobutton(root, text="1.Su peso bajó a menos de 50 kgms", variable=opcion,value=1,command=seleccionar).pack()
+Radiobutton(root, text="2.No se puede donar sangre si la persona ha sido trasplantada, es decir, ha recibido un trasplante de órgano", variable=opcion,value=2, command=seleccionar).pack()
+Radiobutton(root, text="3.Enfermedades como: tuberculosis, cáncer o cualquier enfermedad coronaria.", variable=opcion,value=3, command=seleccionar).pack()   
+Radiobutton(root, text="4.Si el donante esadicto a ningún tipo de droga.", variable=opcion,value=4, command=seleccionar).pack()   
+Radiobutton(root, text="5.Padecióhepatitis B o C.", variable=opcion,value=5, command=seleccionar).pack()   
+Radiobutton(root, text="6.Si has padecido de mal de Chagas no puedes donar.", variable=opcion,value=6, command=seleccionar).pack()
     
 monitor = Label(root)
 monitor.pack()
@@ -32,6 +30,8 @@ monitor.pack()
 root.mainloop()
 ###################################################################################################################
 #
+"""
+"""
 root=Tk()#ventana principal
 root.geometry("600x400")#le da tamaño a la ventana principal
 name_var=StringVar()#almacena el texto que se meta en el cuadro de username
@@ -73,3 +73,49 @@ passw_entry.grid(row=2,column=1)
 sub_btn=Button(root,text = 'Ingresar', command = submit)
 sub_btn.grid(row=3,column=1)
 root.mainloop()#muestra la ventana haciendo un ciclo infinito
+"""
+#COMBOBOX OBTIENE VALOR SELECCIONADO
+import tkinter as tk
+from tkinter import StringVar, ttk
+from tkinter.constants import COMMAND
+from tkinter import messagebox
+def pruebaCombobox():
+    app = tk.Tk() #ventana
+    app.geometry('400x200')#tamaño ventana
+    prueba=StringVar()#manda a pedir el valor seleccionado en el combobox
+    def funcion(recibe):
+        listaJustificaciones=["Su peso bajó a menos de 50 kgms.",
+"No se puede donar sangre si la persona ha sido trasplantada, es decir, ha recibido un trasplante de órgano.",
+"Enfermedades como: tuberculosis, cáncer o cualquier enfermedad coronaria.",
+"Si el donante esadicto a ningún tipo de droga.","Padecióhepatitis B o C.",
+"Si has padecido de mal de Chagas no puedes donar."]
+        x=prueba.get()#manda a pedir el valor seleccionado que está en prueba.
+        if x==listaJustificaciones[0]:
+            print(1,x)
+        elif x==listaJustificaciones[1]:
+            print(2,x)
+        elif x==listaJustificaciones[2]:
+            print(3,x)
+        elif x==listaJustificaciones[3]:
+            print(4,x)
+        elif x==listaJustificaciones[4]:
+            print(5,x)
+        else:
+            print(6,x)
+        print("PREGUNTAR SI CONFIRMA")
+        confirma=messagebox.askquestion("askquestion", "Are you sure?")
+        print(confirma)#retorna yes o no
+        return
+    labelTop = tk.Label(app,text = "Seleccione provincia")
+    labelTop.grid(column=0, row=0,padx=10,pady=10)#ubicación de etiqueta
+
+    comboExample = ttk.Combobox(app,textvariable=prueba,values=["Su peso bajó a menos de 50 kgms."
+    ,"No se puede donar sangre si la persona ha sido trasplantada, es decir, ha recibido un trasplante de órgano.",
+    "Enfermedades como: tuberculosis, cáncer o cualquier enfermedad coronaria.",
+    "Si el donante esadicto a ningún tipo de droga.","Padecióhepatitis B o C.",
+    "Si has padecido de mal de Chagas no puedes donar."])#
+    comboExample.grid(column=0, row=1,padx=10,pady=10)
+    #comboExample.current(0)#pone como determinado alguno de la lista.
+    comboExample.bind("<<ComboboxSelected>>", funcion)
+    app.mainloop()
+pruebaCombobox()
